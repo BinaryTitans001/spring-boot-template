1f264acb57200347b094082b88c90639eab95109
batman003 <wajirc001@gmail.com>
2023-07-29 02:17:22 +0530

    file added

--- src/main/java/by/andd3dfx/templateapp/SpringBootTemplateApplication.java
+++ src/main/java/by/andd3dfx/templateapp/SpringBootTemplateApplication.java
@@ -1,6 +1,5 @@
 package by.andd3dfx.templateapp;
 
-import by.andd3dfx.templateapp.util.StartupHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
@@ -9,9 +8,6 @@ import org.springframework.core.env.Environment;
 public class SpringBootTemplateApplication {
 
 	public static void main(String[] args) {
-		Environment env = new SpringApplication(SpringBootTemplateApplication.class)
-				.run(args)
-				.getEnvironment();
-		StartupHelper.logApplicationStartup(env);
+		SpringApplication.run(SpringBootTemplateApplication.class, args);
 	}
 }
